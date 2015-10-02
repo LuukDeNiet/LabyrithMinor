fd6a9f9f8872e352ad2a9c858bc3b62434b32385
Emptyless <marlo.ploemen@gmail.com>
2015-10-02 12:06:40 +0200

    Script erbij ;)

---
+++ Labyrinth/Assets/Scripts/CameraMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+
+	private Vector3 offset;
+
+	public GameObject labyrinth;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - labyrinth.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.position = labyrinth.transform.position + offset;
+	}
+}
--- Labyrinth/Assets/Scripts/RotationScript.cs
+++ Labyrinth/Assets/Scripts/RotationScript.cs
@@ -5,11 +5,20 @@ public class RotationScript : MonoBehaviour {
 	private float rotateX;
 	private float rotateZ;
 
-	public GameObject player; 
+	//private Vector3 initPosition;
+	//private Vector3 currentPosition;
+
+	//private float angle;
+	//private Vector3 currentRotation;
+
+	//public GameObject player; 
+
+	//public GameObject maincamera;
 
 	// Use this for initialization
 	void Start () {
-	
+		//initPosition = maincamera.transform.position;
+		//currentPosition = initPosition;
 	}
 	
 	// Update is called once per frame
@@ -19,9 +28,21 @@ public class RotationScript : MonoBehaviour {
 
 	// Update is called before each physics update
 	void FixedUpdate() {
-		rotateX = Input.GetAxis ("Horizontal");
-		rotateZ = Input.GetAxis ("Vertical");
+		rotateZ = Input.GetAxis ("Horizontal") * -1;
+		rotateX = Input.GetAxis ("Vertical");
 		transform.Rotate (new Vector3 (rotateX, 0.0f, rotateZ));
+
+
+
+		//maincamera.transform.rotation.ToAngleAxis (out angle, out currentRotation);
+
+		//if (angle >= 45) {
+			//maincamera.transform.Rotate (new Vector3 (rotateX, 0.0f, 0.0f));
+			//currentPosition = currentPosition + new Vector3 (0.0f, rotateX * 0.4f, rotateX * 0.1f);
+			//maincamera.transform.position = currentPosition;
+		//}
+
+
 	}
 
 }
