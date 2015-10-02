5396fc8676fe540c376ce34a10dd17ff3e9fe7dc
Emptyless <marlo.ploemen@gmail.com>
2015-10-02 10:39:03 +0200

    Initieel werkend Rotation script

---
+++ Labyrinth/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour {
+	private float rotateX;
+	private float rotateZ;
+
+	public GameObject player; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Update is called before each physics update
+	void FixedUpdate() {
+		rotateX = Input.GetAxis ("Horizontal");
+		rotateZ = Input.GetAxis ("Vertical");
+		transform.Rotate (new Vector3 (rotateX, 0.0f, rotateZ));
+	}
+
+}
