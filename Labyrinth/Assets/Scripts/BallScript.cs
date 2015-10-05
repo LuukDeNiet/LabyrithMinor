748b209b5b0a965b90c94efd7878fa69748de64b
Luuk de Niet <l.f.deniet93@gmail.com>
2015-10-05 22:43:38 +0200

    Reset level script added to ball prefab

---
+++ Labyrinth/Assets/Scripts/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+
+	private Vector3 start_position_ball;
+	private Quaternion start_rotation_level;
+	private GameObject level;
+
+	// Use this for initialization
+	void Start () {
+		level = GameObject.FindWithTag("Level");
+		start_position_ball = transform.position;
+		start_rotation_level = level.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (transform.position.y <= -10) {
+			Reset ();
+		}
+	}
+
+	void Reset(){
+		level.transform.rotation = start_rotation_level;
+		transform.position = start_position_ball;
+	}
+
+}
