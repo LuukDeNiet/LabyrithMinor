c7e44c785eac525003957506067a7c0ba1d7f5b2
Emptyless <marlo.ploemen@gmail.com>
2015-10-06 22:08:49 +0200

    gyrometer script toegevoegd - dit valt te koppelen aan een rigidbody voor rotatie dmv een gyroscoop

---
+++ AccelerometerTest/Assets/GyrometerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyrometerScript : MonoBehaviour {
+	
+	private Quaternion gyro;
+	private Vector3 rot;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		gyro = Input.gyro.attitude;
+		rot = gyro.eulerAngles;
+		rot = new Vector3 (-rot.x, 0.0f, -rot.y);
+		transform.eulerAngles = rot;
+	}
+}
