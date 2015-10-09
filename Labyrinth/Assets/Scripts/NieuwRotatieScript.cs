2de975cef6a2f641d2073f923ad26ef0f8e04d31
Emptyless <marlo.ploemen@gmail.com>
2015-10-09 13:38:41 +0200

    Commit voor laatste kleine veranderingen + nieuw lelijk level

--- Labyrinth/Assets/Scripts/NieuwRotatieScript.cs
+++ Labyrinth/Assets/Scripts/NieuwRotatieScript.cs
@@ -19,24 +19,37 @@ public class NieuwRotatieScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		ball = GetComponent<Rigidbody> ();
+		if (moveSpeed == 0) {
+			moveSpeed = 250.0f;
+		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		// Make sure it is empty
 		moveVector = Vector3.zero;
+		if ((Input.acceleration.x == 0) && (Input.acceleration.y == 0) && (Input.acceleration.z == 0)) {
 
-		moveX = Input.GetAxis ("Horizontal");
-		moveZ = Input.GetAxis ("Vertical");
-		moveVector = new Vector3 (moveX, 0.0f, moveZ);
+			moveX = Input.GetAxis ("Horizontal");
+			moveZ = Input.GetAxis ("Vertical");
+			moveVector = new Vector3 (moveX, 0.0f, moveZ);
+		
+		} else {
+
+			moveX = Input.acceleration.x;
+			moveZ = Input.acceleration.y;
+			moveVector = new Vector3 (moveX, 0.0f, moveZ) * 5.0f;
+
+		}
 
 		ball.AddForce (moveVector * moveSpeed * Time.deltaTime);
-
+		
 		rotateZ = moveX * 7.0f;
 		rotateX = moveZ * -7.0f;
-
+		
 		// vector3.right = x as, vector3.up = z as
-		gameCamera.MoveRotation(Quaternion.AngleAxis(rotateX, Vector3.right) *  Quaternion.AngleAxis(rotateZ, Vector3.forward));
+		gameCamera.MoveRotation (Quaternion.AngleAxis (rotateX, Vector3.right) * Quaternion.AngleAxis (rotateZ, Vector3.forward));
+
+
 		
 	}
 }
