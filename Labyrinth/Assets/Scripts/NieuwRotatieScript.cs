d5446c5211ab18d79aa32065c96bf591a5437b1a
Emptyless <marlo.ploemen@gmail.com>
2015-10-09 11:16:23 +0200

    HET WERKT!!!

---
+++ Labyrinth/Assets/Scripts/NieuwRotatieScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NieuwRotatieScript : MonoBehaviour {
+
+	public float moveZ; 
+	public float moveX;
+
+	public float rotateZ;
+	public float rotateX;
+
+	private Vector3 moveVector;
+	public float moveSpeed;
+
+	public Rigidbody ball;
+	public Rigidbody gameCamera;
+	
+
+	// Use this for initialization
+	void Start () {
+		ball = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		// Make sure it is empty
+		moveVector = Vector3.zero;
+
+		moveX = Input.GetAxis ("Horizontal");
+		moveZ = Input.GetAxis ("Vertical");
+		moveVector = new Vector3 (moveX, 0.0f, moveZ);
+
+		ball.AddForce (moveVector * moveSpeed * Time.deltaTime);
+
+		rotateZ = moveX * 7.0f;
+		rotateX = moveZ * -7.0f;
+
+		// vector3.right = x as, vector3.up = z as
+		gameCamera.MoveRotation(Quaternion.AngleAxis(rotateX, Vector3.right) *  Quaternion.AngleAxis(rotateZ, Vector3.forward));
+		
+	}
+}
