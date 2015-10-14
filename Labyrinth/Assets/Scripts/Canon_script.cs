a02eba63942673da2139d2a2088686f4012ff3af
Luuk de Niet <l.f.deniet93@gmail.com>
2015-10-14 14:19:37 +0200

    Prefab kanon schiet nu kogels af in de goede richting en ze gaan niet door muren heen. Wel door power-ups, dus oppassen met richten.

---
+++ Labyrinth/Assets/Scripts/Canon_script.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Canon_script : MonoBehaviour {
+
+	private Vector3 canon_position;
+	public Rigidbody bullit;
+	private Vector3 hoek;
+	private float yhoek;
+
+	// Use this for initialization
+	void Start () {
+	
+		InvokeRepeating("Fire", 1, 5f);
+		canon_position = transform.position;
+		canon_position.y = canon_position.y + 2f;
+		hoek = transform.rotation.eulerAngles;
+		yhoek = hoek.y*Mathf.PI/180f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Fire() {
+
+		Rigidbody kanonskogel;
+		kanonskogel = Instantiate(bullit,canon_position,Quaternion.identity) as Rigidbody;
+		kanonskogel.velocity = new Vector3(-Mathf.Cos(yhoek),0,Mathf.Sin(yhoek)) * 10;
+
+	}
+}
---
+++ Labyrinth/Assets/Scripts/bullt_script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullt_script : MonoBehaviour {
+
+	public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision other) 
+	{
+	
+            if(other.gameObject.tag == "Level")
+            {
+				gameObject.SetActive(false);
+			}
+	
+		
+
+			if(other.gameObject.tag == "Player")
+			{
+				//reset en levens -1 of terug naar start
+			}
+
+	}
+}
