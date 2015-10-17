50dd55aceaf1a5cd5dc3630aeff3620421460234
AjdinHusic <ajdin@live.nl>
2015-10-17 14:39:26 +0200

    Finishscript toegevoegd!

---
+++ Labyrinth/Assets/Scripts/FinishScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FinishScript : MonoBehaviour {
+	
+	private int curLevel;
+	public Text curLevelText; 
+
+	// Use this for initialization
+	void Start () 
+	{
+		curLevel = Application.loadedLevel;
+		curLevelText.text = "Level: " + curLevel;
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Finish")) {
+			curLevel ++;
+			Debug.Log(curLevel);
+			Application.LoadLevel(1);
+		}
+
+	}
+	
+}
